2f49d13eb7aaf7cef9906f143572203392bdc9e7
jasonjiangstuy <jasonjiangny@gmail.com>
2021-01-05 07:07:39 -0500

    implemented main and tested

--- PigLatin.java
+++ PigLatin.java
@@ -1,11 +1,25 @@
+import java.util.*;
 public class PigLatin {
     public static void main(String[] args) {
-        
+        Scanner n = new Scanner( System.in );
+        while(n.hasNextLine()){
+            String x = n.nextLine();
+            String output = "";
+            for (String i : x.trim().split("\\s+")){
+                
+                output += pigLatinBest(i) + " ";
+            }
+            if (output.length() > 0){
+                output = output.substring(0, output.length() - 1);
+            }
+            System.out.println(output);
+        }
+        n.close();
     }
 
     public static String pigLatinSimple(String s){//Given a single word of at least 1 character:
-        s.toLowerCase();
-        // if (s.length() >= 1){ 
+        s = s.toLowerCase();
+        if (s.length() >= 1){ 
 
             if (s.charAt(0) == 'a'|| s.charAt(0) == 'e'|| s.charAt(0) == 'i'|| s.charAt(0) == 'o'|| s.charAt(0) == 'u'){
                 s += "hay";
@@ -16,41 +30,43 @@ public class PigLatin {
                 s += "ay";
             }
             
-        // }
+        }else{
+            s += s.charAt(0);
+            s = s.substring(1, s.length());
+            s += "ay";
+        }
 
         return s;
     }
     public static String pigLatin(String s){
-        s.toLowerCase();
+
+        s = s.toLowerCase();
         //Words that start with any digraph (list included for your convenience) move the first two letters to the end, and add 'ay'
         String[] Digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
         boolean wasEdited = false;
-        for (String i : Digraph){
-            if (s.substring(0, 2).equals(i)){
-                s += s.substring(0, 2);
-                s = s.substring(2, s.length());
-                s += "ay";
-                wasEdited = true;
-                break;
+        if (s.length() >= 2){
+            for (String i : Digraph){
+                
+                if (s.substring(0, 2).equals(i)){
+                    s += s.substring(0, 2);
+                    s = s.substring(2, s.length());
+                    s += "ay";
+                    wasEdited = true;
+                    break;
+                }
+                                
             }
         }
-
-        if ((s.charAt(0) == 'a'|| s.charAt(0) == 'e'|| s.charAt(0) == 'i'|| s.charAt(0) == 'o'|| s.charAt(0) == 'u') && !wasEdited){
-            s += "hay";
-            wasEdited = true;
-        }
         if (!wasEdited){
-            s += s.charAt(0);
-            s = s.substring(1, s.length());
-            s += "ay";
+            s = pigLatinSimple(s);
         }
         return s;
     }
 
     public static String pigLatinBest(String s){
-        s.toLowerCase();
+        s = s.toLowerCase();
         if (97 <= (int)s.charAt(0) && (int)s.charAt(0) <= 122){ // if first char is a letter
-            if ((!(97 <= (int)s.charAt(s.length() - 1) && (int)s.charAt(s.length() - 1) <= 122)) && (!(48 <= (int)s.charAt(s.length() - 1) && (int)s.charAt(s.length() - 1) <= 57))){
+            if (!((97 <= (int)s.charAt(s.length() - 1) && (int)s.charAt(s.length() - 1) <= 122) || (48 <= (int)s.charAt(s.length() - 1) && (int)s.charAt(s.length() - 1) <= 57))){
                 //not a letter or number
                 char hold = s.charAt(s.length() - 1);
                 s = s.substring(0, s.length() - 1);
--- Tester.java
+++ Tester.java
@@ -1,13 +1,13 @@
 public class Tester {
     public static void main(String[] args) {
-        String[] simple1 = {"mock", "pie", "david", "aaron"};
-        String[] simple2 = {"ockmay", "iepay", "avidday","aaronhay"};
+        String[] simple1 = {"mock", "pie", "david", "aaron", "a"};
+        String[] simple2 = {"ockmay", "iepay", "avidday","aaronhay", "ahay"};
         for (int i = 0; i < simple1.length; i ++){
             System.out.println(PigLatin.pigLatinSimple(simple1[i]).equals(simple2[i]));
         }
         
-        String[] ok1 = {"the", "check", "skee", "emu", "grade"};
-        String[] ok2 = {"ethay", "eckchay", "eeskay","emuhay", "adegray"};
+        String[] ok1 = {"the", "check", "skee", "emu", "grade", "th", "a"};
+        String[] ok2 = {"ethay", "eckchay", "eeskay","emuhay", "adegray", "thay", "ahay"};
         for (int i = 0; i < ok1.length; i ++){
             if (PigLatin.pigLatin(ok1[i]).equals(ok2[i])){
                 System.out.println("true");    
@@ -16,14 +16,14 @@ public class Tester {
                 System.out.println("false" + PigLatin.pigLatin(ok1[i]));
             }
         }
-        String[] final1 = {"*emu", "4chan", "fish!", "fish", "the.", "cat!", "amazing?", "apple%"};
-        String[] final2 = {"*emu", "4chan", "ishfay!","ishfay", "ethay.", "atcay!", "amazinghay?", "applehay%"};
+        String[] final1 = {"*emu", "4chan", "fish!", "fish", "the.", "cat!", "amazing?", "apple%", "Science!", "poet's"};
+        String[] final2 = {"*emu", "4chan", "ishfay!","ishfay", "ethay.", "atcay!", "amazinghay?", "applehay%", "iencescay!", "oet'spay"};
         for (int i = 0; i < final1.length; i ++){
             if (PigLatin.pigLatinBest(final1[i]).equals(final2[i])){
                 System.out.println("true");    
             }
             else{
-                System.out.println("false" + PigLatin.pigLatinBest(final1[i]));
+                System.out.println("false" + final1[i]+ PigLatin.pigLatinBest(final1[i]));
             }
         }
     }
