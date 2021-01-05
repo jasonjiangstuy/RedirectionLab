74b169c3de0e156350bf141afe75d72cf7079026
jasonjiangstuy <jasonjiangny@gmail.com>
2021-01-05 06:15:25 -0500

    implemented and tested regular pig latin

--- PigLatin.java
+++ PigLatin.java
@@ -18,4 +18,29 @@ public class PigLatin {
         s.toLowerCase();
         return s;
     }
+    public static String pigLatin(String s){
+        //Words that start with any digraph (list included for your convenience) move the first two letters to the end, and add 'ay'
+        String[] Digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+        boolean wasEdited = false;
+        for (String i : Digraph){
+            if (s.substring(0, 2).equals(i)){
+                s += s.substring(0, 2);
+                s = s.substring(2, s.length());
+                s += "ay";
+                wasEdited = true;
+                break;
+            }
+        }
+
+        if ((s.charAt(0) == 'a'|| s.charAt(0) == 'e'|| s.charAt(0) == 'i'|| s.charAt(0) == 'o'|| s.charAt(0) == 'u') && !wasEdited){
+            s += "hay";
+            wasEdited = true;
+        }
+        if (!wasEdited){
+            s += s.charAt(0);
+            s = s.substring(1, s.length());
+            s += "ay";
+        }
+        return s;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,20 @@
 public class Tester {
     public static void main(String[] args) {
-
-        System.out.println(PigLatin.pigLatinSimple("mock"));// → "ockmay"
+        String[] simple1 = {"mock", "pie", "david", "aaron"};
+        String[] simple2 = {"ockmay", "iepay", "avidday","aaronhay"};
+        for (int i = 0; i < simple1.length; i ++){
+            System.out.println(PigLatin.pigLatinSimple(simple1[i]).equals(simple2[i]));
+        }
+        
+        String[] ok1 = {"the", "check", "skee", "emu", "grade"};
+        String[] ok2 = {"ethay", "eckchay", "eeskay","emuhay", "adegray"};
+        for (int i = 0; i < ok1.length; i ++){
+            if (PigLatin.pigLatin(ok1[i]).equals(ok2[i])){
+                System.out.println("true");    
+            }
+            else{
+                System.out.println("false" + PigLatin.pigLatin(ok1[i]));
+            }
+        }
     }
 }
